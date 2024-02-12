20c04477b7a42083a61bb02c1df7e56f378a60cd
Jose Orihuela <freud_camus@hotmail.com>
2024-02-11 20:21:11 -0500

    astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-  // site: '',
-  // base: './'
+  integrations: [tailwind()],
+  site: 'https://Jose-Orihuela-Ponce.github.io',
+  base: '/porta'
 });
